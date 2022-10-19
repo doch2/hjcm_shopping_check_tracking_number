cac6460131a461b160c99df751e1fb54f91c73c5
doch13_ <dohui0922@dimigo.hs.kr>
2022-10-19 18:20:42 +0900

    feat: Tracking Number Checking Service

---
+++ README.md
@@ -0,0 +1,10 @@
+# hjcm_shopping_check_tracking_number
+
+고객의 운송장 정보를 알려주는 서비스입니다.
+
+Flask를 통해 제작하였으며, Docker Container로 만들어 CloudType에 배포하였습니다.
+
+
+## 개발을 위해선
+
+Firebase Dashboard에서 Admin Key 파일을 다운받아 'firebase-adminkey.json' 이름으로 프로젝트 경로 제일 상단에 파일을 저장하여야 합니다.
---
+++ server.py
@@ -0,0 +1,42 @@
+from flask import Flask, request, render_template
+from flask_cors import CORS
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+import random
+
+
+app = Flask(__name__)
+CORS(app)
+
+cred = credentials.Certificate('firebase-adminkey.json')
+firebase_admin.initialize_app(cred,{
+    'databaseURL' : 'https://hjcm-shopping-default-rtdb.asia-southeast1.firebasedatabase.app'
+})
+
+
+
+@app.route("/")
+def searchPage():
+    lotto_li = list(range(1,46))
+    lotto = sorted(random.sample(lotto_li, 6))
+    return render_template("search-number.html", variable=lotto)
+
+@app.route("/result")
+def resultPage():
+    phoneNum = request.args.get('phoneNum')
+
+    ref = db.reference('trackingNumberList')
+    
+    dbInfo = ref.get(phoneNum)
+    dbInfo = dbInfo['010-7916-2095']
+    
+    handOverInfo = [dbInfo['name'], dbInfo['trackingNum']]
+
+    print(dbInfo)
+    return render_template("result.html", variable=handOverInfo)
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
